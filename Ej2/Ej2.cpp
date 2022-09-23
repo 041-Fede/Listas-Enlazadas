8d161a8cec2b32640ca9a3f76654f4ee1f2adb6e
Federico Nahuel Encinaz Sayago <101824879+041-Fede@users.noreply.github.com>
2022-09-23 16:37:10 -0300

    Update Ej2.cpp

--- Ej2/Ej2.cpp
+++ Ej2/Ej2.cpp
@@ -1,3 +1,11 @@
+/*Se tiene lista enlazada donde cada nodo contiene nombre y número de documento de las personas inscriptas en un curso, ordenada por número de documento.
+Hacer funciones para:
+a. agregar un inscripto al curso, considerar que puede estar la lista vacía o ya contener información.
+b. dar de baja una persona  que no concurrirá al curso. 
+c. imprimir la lista completa.
+d. Imprimir la lista en orden inverso (ordenada por documento de mayor a menor)*/
+
+
 #include <iostream>
 
 using namespace std;
